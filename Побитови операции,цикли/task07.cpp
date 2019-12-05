11a38ee5395242126bde07beb00bb49de04907e5
MarioStoev99 <56639678+MarioStoev99@users.noreply.github.com>
2019-12-05 18:22:48 +0200

    Update task07.cpp

--- Побитови операции,цикли/task07.cpp
+++ Побитови операции,цикли/task07.cpp
@@ -1,1 +1,25 @@
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int max = 0;
+    int count = 0;
+    while(true)
+    {
+        cin >> n;
+        if(max > n)
+        {
+            cout << count;
+            break;
+        }
+        else
+        {
+            count++;
+            max = n;
+        }
+    }
+    return 0;
+}
