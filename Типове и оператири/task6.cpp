227e3c04c8b24baf372d033fe382708761fb780e
MarioStoev99 <56639678+MarioStoev99@users.noreply.github.com>
2019-12-05 18:09:02 +0200

    Create task6.cpp

---
+++ Типове и оператири/task6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int p,q;
+    cin >> p >> q;
+    cout << "quotient = " << p / q << " , " << "remainder = " << p % q;
+    return 0;
+}
