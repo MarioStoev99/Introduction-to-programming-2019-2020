67da8f70e10a95719ced78bf180292da3265f9c2
MarioStoev99 <56639678+MarioStoev99@users.noreply.github.com>
2019-12-05 18:35:32 +0200

    Create task4.cpp

---
+++ Вложени цикли/task4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int i = 100;i < 1000;i++)
+    {
+        if((i % 10 != i / 10 % 10) && (i % 10 != i / 100) && (i / 10 % 10 != i / 100))
+            cout << i << " ";
+    }
+    return 0;
+}
