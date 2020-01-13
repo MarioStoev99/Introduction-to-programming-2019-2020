161467bd6a21712694652fc6c2bbf99fcf46dae2
MarioStoev99 <56639678+MarioStoev99@users.noreply.github.com>
2020-01-13 17:49:01 +0200

    Create task01.cpp

---
+++ Рекурсия/task01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int rec(int n)
+{
+    if(n == 0)
+        return 1;
+    else
+        return n*rec(n - 1);
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << rec(n);
+    return 0;
+}
